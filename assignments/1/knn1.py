Fixed cosine distance formula, add code to create gif for regression, added code to find best k

assignments/1/knn1.py
@@ -3,142 +3,211 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import sys
+import time
+from sklearn.neighbors import KNeighborsClassifier
 
 sys.path.append('./../../')
 from models.knn.knn import KNN
 from performance_measures.knn_score import Scores
 
-# Task 1
+# # Task 1
 
+# # Load data
+# df = pd.read_csv("./../../data/external/spotify.csv")
+# df = df.drop(columns=['Unnamed: 0'])
+# df = df.drop_duplicates(subset='track_id', keep="first")
+# df_numerical = df.select_dtypes(include=['number'])
+
+# # normalizing data
+# def normalize(df):
+#     return (df - df.min()) / (df.max() - df.min())
+# df_numerical = normalize(df_numerical)
+
+# # Plotting data
+
+# # Historgram
+# # df_numerical.hist(bins=50, figsize=(15, 10))
+# # plt.show()
+
+# # Boxplot
+# # df_numerical.boxplot(figsize=(15, 10))
+# # plt.show()
+
+# # Violin plot
+# # sns.violinplot(data=df_numerical)
+# # plt.show()
+
+# # Pairwise Scatterplot
+# # sns.pairplot(df_numerical)
+# # plt.show()
+
+
+# # Task 3
+
+# # suffle the data
+# df = df.sample(frac=1).reset_index(drop=True)
+
+# # split the data
+# train_size = int(0.8 * len(df))
+# validate_size = int(0.1 * len(df))
+# test_size = len(df) - train_size - validate_size
+
+# df = df.drop(columns=['key', 'mode'])
+# df_train, df_validate, df_test = np.split(df, [train_size, train_size + validate_size])
+
+
+# # Load training data
+# x_train = df_train.select_dtypes(include=['number'])
+# # x_train.drop_duplicates(subset='track_id', keep='first')
+# y_train = df_train['track_genre']
+
+# # Load test data
+# x_test = df_test.select_dtypes(include=['number'])
+# # x_test.drop_duplicates(subset='track_id', keep='first')
+# y_test = df_test['track_genre']
+
+# # Load validation data
+# x_validate = df_validate.select_dtypes(include=['number'])
+# # x_validate.drop_duplicates(subset='track_id', keep='first')
+# y_validate = df_validate['track_genre']
+
+# # Normalize data
+# x_train = normalize(x_train)
+# x_test = normalize(x_test)
+# x_validate = normalize(x_validate)
+
+# # Fit KNN model
+# k = 30
+# knn = KNN(k)
+# knn.fit(x_train, y_train)
+
+# knn_sklearn = KNeighborsClassifier(n_neighbors=k)
+# knn_sklearn.fit(x_train, y_train)
+
+# # taking only 1000 samples for faster runtime 
+# # x_validate = x_validate.iloc[:10]
+# # y_validate = y_validate.iloc[:10]
+
+# start_myknn = time.time()
+# print("Predicting")
+# y_pred = knn.predict(x_validate, distance_metric='cosine')
+# print(np.mean(y_pred == y_validate))
+# end_myknn = time.time()
+# print(f"Time taken by my KNN: {end_myknn - start_myknn}")
+
+# start_sklearn = time.time()
+# y_pred_sklearn = knn_sklearn.predict(x_validate)
+# print(np.mean(y_pred_sklearn == y_validate))
+# end_sklearn = time.time()
+# print(f"Time taken by sklearn KNN: {end_sklearn - start_sklearn}")
+
+# # tune for k
+# def tune_k(k_values, x_validate, y_validate, distance_metric):
+#     accuracy_values = []
+#     for k in k_values:
+#         knn.k = k
+#         y_pred = knn.predict(x_validate, distance_metric=distance_metric)
+#         accuracy = np.mean(y_pred == y_validate)
+#         accuracy = int(accuracy * 100)
+#         accuracy_values.append((accuracy, (k, distance_metric)))
+#     return accuracy_values
+
+# k_values = range(10, 100, 10)
+
+# # manhattan tuning for k
+# accuracy_values_manhattan = tune_k(k_values, x_validate, y_validate, 'manhattan')
+    
+# # euclidean tuning for k
+# accuracy_values_euclidean = tune_k(k_values, x_validate, y_validate, 'euclidean')
+
+# # cosine tuning for k
+# accuracy_values_cosine = tune_k(k_values, x_validate, y_validate, 'cosine')
+
+
+# all_pairs = accuracy_values_manhattan + accuracy_values_euclidean + accuracy_values_cosine
+
+# all_pairs.sort(reverse=True)
+# print("Top 10 pairs")
+# print(all_pairs[:10])
+
+# # k vs accuracy plot
+
+# def plot_k_vs_accuracy(accuracy_values_pairs):
+#     distance_metric = accuracy_values_pairs[0][1][1]
+#     accuracy_values = [x[0] for x in accuracy_values_pairs]
+#     k_values = [x[1][0] for x in accuracy_values_pairs]
+#     plt.plot(k_values, accuracy_values)
+#     plt.xlabel('k')
+#     plt.ylabel('accuracy')
+#     plt.title(f'k vs accuracy for {distance_metric}')
+#     plt.show()
+
+# # plot_k_vs_accuracy(accuracy_values_manhattan)
+# # plot_k_vs_accuracy(accuracy_values_euclidean)
+# # plot_k_vs_accuracy(accuracy_values_cosine)
+
+# # Test for best k and distance metric
+# best_k = all_pairs[0][1][0]
+# best_distance_metric = all_pairs[0][1][1]
+
+# knn.k = best_k
+# y_pred = knn.predict(x_test, distance_metric=best_distance_metric)
+# accuracy = np.mean(y_pred == y_test)
+# print(f"Accuracy for test data for best k: {best_k} and distance metric: {best_distance_metric} is {accuracy*100}%")
+
+# # Calculate scores
+# scores = Scores(y_test, y_pred)
+# print(f"Accuracy: {scores.accuracy}")
+# print(f"Micro Precision: {scores.micro_precision}")
+# print(f"Macro Precision: {scores.macro_precision}")
+# print(f"Micro Recall: {scores.micro_recall}")
+# print(f"Macro Recall: {scores.macro_recall}")
+# print(f"Micro F1 Score: {scores.micro_f1}")
+# print(f"Macro F1 Score: {scores.macro_f1}")
+
+# Second data set
 # Load data
-df = pd.read_csv("./../../data/external/spotify.csv")
-df = df.drop(columns=['Unnamed: 0'])
-df = df.drop_duplicates(subset='track_id', keep="first")
-df_numerical = df.select_dtypes(include=['number'])
+df_train = pd.read_csv("./../../data/external/spotify-2/train.csv")
+df_validate = pd.read_csv("./../../data/external/spotify-2/validate.csv")
+df_test = pd.read_csv("./../../data/external/spotify-2/test.csv")
 
-# normalizing data
-def normalize(df):
-    return (df - df.min()) / (df.max() - df.min())
-df_numerical = normalize(df_numerical)
+df_train = df_train.drop_duplicates(subset='track_id', keep='first')
+df_validate = df_validate.drop_duplicates(subset='track_id', keep='first')
+df_test = df_test.drop_duplicates(subset='track_id', keep='first')
 
-# Plotting data
+df_train.drop(columns=['Unnamed: 0'])
+df_validate.drop(columns=['Unnamed: 0'])
+df_test.drop(columns=['Unnamed: 0'])
 
-# Historgram
-# df_numerical.hist(bins=50, figsize=(15, 10))
-# plt.show()
+df_train = df_train.drop(columns=['key', 'mode'])
+df_validate = df_validate.drop(columns=['key', 'mode'])
+df_test = df_test.drop(columns=['key', 'mode'])
 
-# Boxplot
-# df_numerical.boxplot(figsize=(15, 10))
-# plt.show()
-
-# Violin plot
-# sns.violinplot(data=df_numerical)
-# plt.show()
-
-# Pairwise Scatterplot
-# sns.pairplot(df_numerical)
-# plt.show()
-
-
-# Task 3
-
-# suffle the data
-df = df.sample(frac=1).reset_index(drop=True)
-
-# split the data
-train_size = int(0.8 * len(df))
-validate_size = int(0.1 * len(df))
-test_size = len(df) - train_size - validate_size
-
-df = df.drop(columns=['key', 'mode'])
-df_train, df_validate, df_test = np.split(df, [train_size, train_size + validate_size])
-
-
-# Load training data
 x_train = df_train.select_dtypes(include=['number'])
-# x_train.drop_duplicates(subset='track_id', keep='first')
 y_train = df_train['track_genre']
 
-# Load test data
-x_test = df_test.select_dtypes(include=['number'])
-# x_test.drop_duplicates(subset='track_id', keep='first')
-y_test = df_test['track_genre']
-
-# Load validation data
 x_validate = df_validate.select_dtypes(include=['number'])
-# x_validate.drop_duplicates(subset='track_id', keep='first')
 y_validate = df_validate['track_genre']
 
-# Normalize data
-x_train = normalize(x_train)
-x_test = normalize(x_test)
-x_validate = normalize(x_validate)
+x_test = df_test.select_dtypes(include=['number'])
+y_test = df_test['track_genre']
 
-# Fit KNN model
-k = 75
-knn = KNN(k)
-knn.fit(x_train, y_train)
-
-
-# taking only 100 samples for faster runtime 
-x_validate = x_validate.iloc[:100]
-y_validate = y_validate.iloc[:100]
-
-y_pred = knn.predict(x_validate, distance_metric='euclidean')
-print(np.mean(y_pred == y_validate))
-
-
-
-# tune for k
-def tune_k(k_values, x_validate, y_validate, distance_metric):
-    accuracy_values = []
-    for k in k_values:
-        knn.k = k
-        y_pred = knn.predict(x_validate, distance_metric=distance_metric)
-        accuracy = np.mean(y_pred == y_validate)
-        accuracy = int(accuracy * 100)
-        accuracy_values.append((accuracy, (k, distance_metric)))
-    return accuracy_values
-
-k_values = range(10, 100, 10)
-
-# manhattan tuning for k
-accuracy_values_manhattan = tune_k(k_values, x_validate, y_validate, 'manhattan')
-    
-# euclidean tuning for k
-accuracy_values_euclidean = tune_k(k_values, x_validate, y_validate, 'euclidean')
-
-# cosine tuning for k
-accuracy_values_cosine = tune_k(k_values, x_validate, y_validate, 'cosine')
-
-
-all_pairs = accuracy_values_manhattan + accuracy_values_euclidean + accuracy_values_cosine
-
-all_pairs.sort(reverse=True)
-print("Top 10 pairs")
-print(all_pairs[:10])
-
-# k vs accuracy plot
-
-def plot_k_vs_accuracy(accuracy_values_pairs):
-    distance_metric = accuracy_values_pairs[0][1][1]
-    accuracy_values = [x[0] for x in accuracy_values_pairs]
-    k_values = [x[1][0] for x in accuracy_values_pairs]
-    plt.plot(k_values, accuracy_values)
-    plt.xlabel('k')
-    plt.ylabel('accuracy')
-    plt.title(f'k vs accuracy for {distance_metric}')
-    plt.show()
-
-plot_k_vs_accuracy(accuracy_values_manhattan)
-plot_k_vs_accuracy(accuracy_values_euclidean)
-plot_k_vs_accuracy(accuracy_values_cosine)
-
-# Test for best k and distance metric
-best_k = all_pairs[0][1][0]
-best_distance_metric = all_pairs[0][1][1]
-
+best_k = 60
+best_distance_metric = 'euclidean'
+knn = KNN(60)
 knn.k = best_k
-y_pred = knn.predict(x_test, distance_metric=best_distance_metric)
-accuracy = np.mean(y_pred == y_test)
-print(f"Accuracy for test data for best k: {best_k} and distance metric: {best_distance_metric} is {accuracy*100}%")
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_validate, distance_metric=best_distance_metric)
+accuracy = np.mean(y_pred == y_validate)
+
+print(f"Accuracy for validation data for best k: {best_k} and distance metric: {best_distance_metric} is {accuracy*100}%")
+
+# Calculate scores
+scores = Scores(y_validate, y_pred)
+print(f"Accuracy: {scores.accuracy}")
+print(f"Micro Precision: {scores.micro_precision}")
+print(f"Macro Precision: {scores.macro_precision}")
+print(f"Micro Recall: {scores.micro_recall}")
+print(f"Macro Recall: {scores.macro_recall}")
+print(f"Micro F1 Score: {scores.micro_f1}")
+print(f"Macro F1 Score: {scores.macro_f1}")

assignments/1/regression1.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import imageio
+
 
 sys.path.append('./../../')
 from models.linear_regression.linear_regression import LinearRegression
@@ -12,15 +14,8 @@ from performance_measures.regression_score import Scores
 # Load data
 df = pd.read_csv("./../../data/external/linreg.csv")
 
-# Plotting data
-plt.scatter(df['x'], df['y'])
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Scatter plot of X and Y')
-plt.show()
-
 # initialize model
-model = LinearRegression(n_iterations=1000, learning_rate=0.01)
+model = LinearRegression(learning_rate=0.01)
 
 # Suffle and split the data
 
@@ -37,52 +32,81 @@ x_train = df_train['x'].to_numpy()
 y_train = df_train['y'].to_numpy()
 
 # Fit the model
-model.fit(x_train, y_train)
+model.fit(x_train, y_train, create_gif=True)
 
-# Predict
-y_pred = model.predict(x_train)
+def create_gif(n_images):
+    frames = []
+    for i in range(n_images):
+        img_path = f'./figures/gif_images/{i}.png'
+        frame = imageio.imread(img_path)
+        frames.append(frame)
+    gif_path = './figures/linear_regression.gif'
+    imageio.mimsave(gif_path, frames, fps=10)
+   
+# n_images = model.epock_to_converge // 100
+# create_gif(n_images)
+
+y_train = y_train[np.argsort(x_train)]
+x_train = np.sort(x_train)
 
 # Plot the regression line
 plt.scatter(x_train, y_train)
-plt.plot(x_train, y_pred, color='red')
+plt.plot(x_train, model.get_line(x_train), color='red')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Regression line')
 plt.show()
-
-# Calculate scores
-scores = Scores(y_train, y_pred)
-
-print(f"Mean Squared Error: {scores.mse}")
 
 
 # Task 2: Degree k
 
 # initialize model
 
-model = LinearRegression(n_iterations=10000, learning_rate=0.01, k=10)
+model = LinearRegression(learning_rate=0.01, k=20)
 
 # Fit the model
-model.fit(x_train, y_train)
+model.fit(x_train, y_train, create_gif=True)
 
-# Predict
-y_pred = model.predict(x_train)
+n_images = model.epock_to_converge // 100
+create_gif(n_images)
 
-# sort values
-y_pred = y_pred[np.argsort(x_train)]
 y_train = y_train[np.argsort(x_train)]
 x_train = np.sort(x_train)
 
 # Plot the regression line
 plt.scatter(x_train, y_train)
-plt.plot(x_train, y_pred, color='red')
+plt.plot(x_train, model.get_line(x_train), color='red')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Regression line')
 plt.show()
 
-# Calculate scores
-scores = Scores(y_train, y_pred)
 
-print(f"Mean Squared Error: {scores.mse}")
+# Tune for Hyperparameter
 
+# Load validation data
+x_validate = df_validate['x'].to_numpy()
+y_validate = df_validate['y'].to_numpy()
+
+# Fit the model
+model.fit(x_validate, y_validate)
+
+min_error = float('inf')
+best_k = 0
+mse_list = []
+for k in range(1, 21):
+    model = LinearRegression(learning_rate=0.01, k=k, error_threshold=1e-9)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_validate)
+    score = Scores(y_validate, y_pred)
+    mse = score.mse
+    mse = mse.round(9)
+    mse_list.append((mse,k))
+    if mse < min_error:
+        min_error = mse
+        best_k = k
+
+print(f'Best k: {best_k}')
+print(f'Minimum MSE: {min_error}')
+
+mse_list = sorted(mse_list)

models/knn/knn.py
@@ -5,12 +5,14 @@ class KNN:
         self.k = k
 
     def fit(self, x_train, y_train):
-        self.x_train = x_train
-        self.y_train = y_train
+        self.x_train = x_train.to_numpy()
+        self.y_train = y_train.to_numpy()
+        self.norm_x_train = np.linalg.norm(self.x_train, axis=1)
         
     def predict(self, x_test, distance_metric='euclidean'):
         y_pred = []
-        for x in x_test.values:
+        x_test = x_test.to_numpy()
+        for x in x_test:
             y_pred.append(self.predict_one(x, distance_metric))
         return np.array(y_pred)
     
@@ -20,7 +22,14 @@ class KNN:
         elif distance_metric == 'manhattan':
             distances = np.sum(np.abs(self.x_train - x), axis=1)
         elif distance_metric == 'cosine':
-            distances = np.dot(self.x_train, x) / (np.linalg.norm(self.x_train) * np.linalg.norm(x))
-        nearest_indices = np.argsort(distances)[:self.k]
-        nearest_labels = self.y_train.iloc[nearest_indices]
-        return nearest_labels.mode().values[0]
+            # For this looked up online resourses for how to calculate cosine distance efficiently
+            dot_product = np.dot(self.x_train, x)
+            norm_x = np.linalg.norm(x)
+            cosine_similarity = dot_product / (norm_x * self.norm_x_train)
+            distances = 1 - cosine_similarity
+
+        # For this also looked up online resourses for how to get k nearest neighbours efficiently  
+        nearest_indices = np.argpartition(distances, self.k)[:self.k]
+        nearest_labels = self.y_train[nearest_indices]      
+        index, counts = np.unique(nearest_labels, return_counts=True)
+        return index[np.argmax(counts)]

models/linear_regression/linear_regression.py
@@ -1,29 +1,55 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class LinearRegression:
-    def __init__(self, n_iterations=100, learning_rate=0.001, k = 1 ,lambda_= 0.0):
-        self.n_iterations = n_iterations
+    def __init__(self, n_Epochs=10000, learning_rate=0.001, k = 1, error_threshold=1e-6):
+        self.n_Epochs = n_Epochs
         self.learning_rate = learning_rate
-        self.lambda_ = lambda_
         self.k = k
+        self.mse_list = []
+        self.variance_list = []
+        self.std_list = []
+        self.epock_to_converge = n_Epochs
+        self.error_threshold = error_threshold
     
-    def fit(self, x_train, y_train):
+    def fit(self, x_train, y_train, create_gif=False):
+        self.y_train = y_train
+        self.y_train = self.y_train[np.argsort(x_train)]
+        x_train = np.sort(x_train)
         self.x_train = x_train.reshape(-1,1)
+        if create_gif:
+            self.x_train_gif = self.x_train.copy()
+
         x_train = x_train.reshape(-1,1)
         for i in range(2,self.k+1):
             temp = x_train ** i
             self.x_train = np.hstack((self.x_train,temp))
 
-        self.y_train = y_train
         self.n_samples = x_train.shape[0]
         self.n_features = self.k
         self.weights = np.zeros(self.n_features)
         self.bias = 0
-        for i in range(self.n_iterations):
+        for i in range(self.n_Epochs):
             self.update_weights()
+
+            if (i>2):
+                length = len(self.mse_list)
+                if (abs(self.mse_list[length-1] - self.mse_list[length-2])<self.error_threshold):
+                    self.epock_to_converge = i
+                    break
+                    
+            if create_gif:
+                if (i%100 == 0):
+                    self.save_image(f'./figures/gif_images/{i//100}.png')
 
     def update_weights(self):
         y_pred = self.predict(self.x_train)
+        mse = np.mean((self.y_train - y_pred) ** 2)
+        std = np.std(y_pred)
+        variance = np.var(y_pred)
+        self.mse_list.append(mse)
+        self.variance_list.append(variance)
+        self.std_list.append(std)
         dW = (1/self.n_samples) * np.dot(self.x_train.T , (y_pred - self.y_train))
         self.weights -= self.learning_rate * dW
         db = (1/self.n_samples) * np.sum(y_pred - self.y_train)
@@ -38,3 +64,36 @@ class LinearRegression:
                 xc = np.hstack((xc,temp))
             x = xc
         return np.dot(x,self.weights) + self.bias
+    
+    def get_line(self, x):
+        w = self.weights
+        b = self.bias
+        n = len(w)
+        y = b
+        for i in range(n):
+            y += w[i] * x ** (i+1)
+        return y
+    
+    def save_image(self,name):
+        y = self.y_train
+        x = self.x_train_gif
+        fig , axs = plt.subplots(2,2, figsize=(10,10))
+        axs[0,0].scatter(x, y)
+        axs[0,0].plot(x, self.get_line(x), color='red')
+        axs[0,0].set_xlabel('X')
+        axs[0,0].set_ylabel('Y')
+        axs[0,0].set_title('Regression line')
+        axs[0,1].plot(self.mse_list)
+        axs[0,1].set_xlabel('Epochs')
+        axs[0,1].set_ylabel('MSE')
+        axs[0,1].set_title('Epochs vs MSE')
+        axs[1,0].plot(self.variance_list)
+        axs[1,0].set_xlabel('Epochs')
+        axs[1,0].set_ylabel('Variance')
+        axs[1,0].set_title('Epochs vs Variance')
+        axs[1,1].plot(self.std_list)
+        axs[1,1].set_xlabel('Epochs')
+        axs[1,1].set_ylabel('Standard Deviation')
+        axs[1,1].set_title('Epochs vs Standard Deviation')
+        plt.savefig(name)
+        plt.close()